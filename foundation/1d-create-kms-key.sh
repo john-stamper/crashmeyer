31d1504968b3cb00508a8364d9a6525fa36c6702
John Stamper <stamperj@google.com>
2025-10-09 05:23:27 -0400

    updated 1d to insert kms key uri to bq remote fx env var

--- foundation/1d-create-kms-key.sh
+++ foundation/1d-create-kms-key.sh
@@ -5,9 +5,9 @@ KEY_NAME="symmetric-encrypt-decrypt"
 
 gcloud kms keyrings create $KEY_RING --location global
 gcloud kms keys create $KEY_NAME \
-	--keyring $KEY_RING \
-	--location global \
-	--purpose "encryption"
+        --keyring $KEY_RING \
+        --location global \
+        --purpose "encryption"
 
 KMS_KEY_URI=`gcloud kms keys list --keyring $KEY_RING --location global --format="value(NAME)"`
-echo "\t--set-env-vars=KMS_KEY_URI=$KMS_KEY_URI"
+echo "--set-env-vars=KMS_KEY_URI=gcp-kms://$KMS_KEY_URI" >> ../bq-remote-fx/deploy.sh
