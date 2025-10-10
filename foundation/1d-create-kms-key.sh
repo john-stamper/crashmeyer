deaf28039cbab2c8a4bd05a0b8cf3579ee381b4e
John Stamper <stamperj@google.com>
2025-10-10 04:57:06 -0400

    fixing config values

--- foundation/1d-create-kms-key.sh
+++ foundation/1d-create-kms-key.sh
@@ -11,3 +11,4 @@ gcloud kms keys create $KEY_NAME \
 
 KMS_KEY_URI=`gcloud kms keys list --keyring $KEY_RING --location global --format="value(NAME)"`
 echo "--set-env-vars=KMS_KEY_URI=gcp-kms://$KMS_KEY_URI" >> ../bq-remote-fx/deploy.sh
+echo "kms.keyuri=gcp-kms://$KMS_KEY_URI" >> ../sql-app/src/main/resources/config.properties
