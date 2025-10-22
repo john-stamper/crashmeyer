0aab3cb5660c1ff2cbe35e24df8089346b87a156
John Stamper <stamperj@google.com>
2025-10-22 11:13:33 -0400

    adding sqlclient role to bq connection svc acct

--- outbound/3a-create-bq-sql-connection.sh
+++ outbound/3a-create-bq-sql-connection.sh
@@ -4,8 +4,14 @@ DISPLAY_NAME="crashmeyer"
 SQL_INSTANCE_ID=`gcloud sql instances describe crashmeyer-instance --format="value(connectionName)"`
 DB_PASSWORD=`gcloud secrets versions access latest --secret=db-password`
 PROJECT_ID=`gcloud config list --format="value(core.project)"`
+PROJECT_NUMBER=`gcloud projects describe $PROJECT_ID --format="value(projectNumber)"`
 
 bq mk --connection --display_name="$DISPLAY_NAME" --connection_type='CLOUD_SQL' \
       --properties='{"instanceId":"'"$SQL_INSTANCE_ID"'","database":"acme","type":"POSTGRES"}' --connection_credential='{"username":"postgres","password":"'"$DB_PASSWORD"'"}' \
       --project_id="$PROJECT_ID" --location='us' \
       crashmeyer-acme-connection
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:service-$PROJECT_NUMBER@gcp-sa-bigqueryconnecition.iam.gserviceaccount.com" \
+  --role="roles/cloudsql.client" \
+  --quiet
